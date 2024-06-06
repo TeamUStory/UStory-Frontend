fbb79593a07bd01d9a3a99fc5e43126919de8dac
양혜지 <97512098+hyeji17@users.noreply.github.com>
2024-06-06 22:04:15 +0900

    🚩 Feature: 캐러셀 기능 코드 수정

--- src/components/Carousel/CarouselItem.jsx
+++ src/components/Carousel/CarouselItem.jsx
@@ -1,18 +1,20 @@
 import styles from './Carousel.module.scss';
-import {Carousel} from './Carousel';
 import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import { CarouselContext } from './Carousel';
 
-const CarouselItem = ({ children }) => {
-  return (
+export const CarouselItem = ({ index, children }) => {
+  const currentIndex = useContext(CarouselContext);
+
+  return index === currentIndex ? (
     <div className={styles.carousel_item}>
       {children}
     </div>
-  );
+  ) : null;
 };
 
-Carousel.Item = CarouselItem;
-
 CarouselItem.propTypes = {
+  index: PropTypes.number.isRequired,
   children: PropTypes.node,
 };
 
