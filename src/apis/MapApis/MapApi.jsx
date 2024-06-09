2b8c693a8a50d741499d4599f489d9a0183c64bb
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-06-09 20:08:52 +0900

    feat: Kakao API 지도 클러스터 적용

--- src/apis/MapApis/MapApi.jsx
+++ src/apis/MapApis/MapApi.jsx
@@ -8,26 +8,51 @@ const MapApi = ({ width = '100%', height = '344px', borderRadius = "20px" }) => 
         // Kakao 지도 API 스크립트 동적으로 추가
         const script = document.createElement('script');
         script.async = true;
-        script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_MAP_API_KEY}&autoload=false`;
+        script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_MAP_API_KEY}&autoload=false&libraries=services,clusterer,drawing`;
         document.body.appendChild(script);
 
         // 스크립트가 로드된 후에 지도를 생성하도록 함
         script.onload = () => {
             window.kakao.maps.load(() => {
                 if (containerRef.current) {
-                    const mapOption = {
-                        center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-                        level: 9, // 지도의 확대 레벨
-                    };
-                    const map = new window.kakao.maps.Map(containerRef.current, mapOption);
+                    var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div
+                        center : new kakao.maps.LatLng(36.2683, 127.6358), // 지도의 중심좌표 
+                        level : 14 // 지도의 확대 레벨 
+                    });
 
-                    // 크기를 변경한 이후에는 반드시 map.relayout 함수를 호출
-                    const relayout = () => {
-                        map.relayout();
-                    };
+                    var clusterer = new kakao.maps.MarkerClusterer({
+                        map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+                        averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+                        minLevel: 10 // 클러스터 할 최소 지도 레벨 
+                    });
+                
 
-                    // 예시: 지도 크기 변경 후 재배치
-                    relayout();
+                    function addClusterMarker (diaryImage, x, y) {
+                        var marker = new kakao.maps.Marker({
+                            position: new kakao.maps.LatLng(x, y)
+                        });
+                        var markerImage = new kakao.maps.MarkerImage(
+                            diaryImage,
+                            new kakao.maps.Size(40, 40)
+                        );
+                        marker.setImage(markerImage);
+                        
+                        clusterer.addMarker(marker);
+                    }
+                    
+                    // 테스트 데이터
+                    addClusterMarker('https://i.namu.wiki/i/HkXddAbypRkTfnkC6Zg8tt7WgseYiFc8lqY3GI3p9YkcrW6NPS9Eo4iT3GudyOz6qr5IUwArV3DZu1IydH9YbA.webp', 37.23, 126.67);
+                    addClusterMarker('https://i.namu.wiki/i/4c9RUCMb7AZfT1DL15AcE-ZBQ_K7oDSkS9tDBAsxD93X7ZWoETUhRxL07fy5WLzE1blO8R7lxzCRCeQJ230bLg.webp', 37.10, 126.50);
+                    addClusterMarker('https://i.namu.wiki/i/lT2DdjdyqFE5IuD_9uuyusuGd7EpgisaYSuVb-lwdoVZFHf2yh0nD5aNe4aLz7prnXKp5MhJl5WbCRHZJ0DPVA.webp', 37.30, 126.77);
+
+                    
+                    // // 크기를 변경한 이후에는 반드시 map.relayout 함수를 호출
+                    // const relayout = () => {
+                    //     map.relayout();
+                    // };
+
+                    // // 예시: 지도 크기 변경 후 재배치
+                    // relayout();
                 }
             });
         };
