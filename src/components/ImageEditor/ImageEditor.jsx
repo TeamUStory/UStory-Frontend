d873e6fd579bb9328ca5e8ee5402e1f78dc80658
양혜지 <97512098+hyeji17@users.noreply.github.com>
2024-06-11 17:45:31 +0900

    ✨ Feat: 이미지 크롭 기능 코드 수정

--- src/components/ImageEditor/ImageEditor.jsx
+++ src/components/ImageEditor/ImageEditor.jsx
@@ -1,64 +1,60 @@
-import { useState, useRef } from 'react';
+import { useRef } from 'react';
 import Cropper from 'react-cropper';
 import 'cropperjs/dist/cropper.css';
 import PropTypes from 'prop-types';
+import Button from '@/components/Button/Button';
+import Modal from '@/components/Modal/Modal';
+import './ImageEditor.scss';
 
-const ImageEditor = ({ uploadedImage }) => {
-  const [cropData, setCropData] = useState("#");
-  const cropperRef = useRef(null);
-  
-  const getCropData = () => {
-    if (cropperRef.current && typeof cropperRef.current.getCroppedCanvas === 'function') {
-      const croppedCanvas = cropperRef.current.getCroppedCanvas();
-      if (croppedCanvas) {
-        const dataUrl = croppedCanvas.toDataURL();
-        setCropData(dataUrl);
-      }
-    }
-  };
-        
-  console.log(uploadedImage); 
+const ImageEditor = ({ uploadedImage, closeModal, setCroppedImage }) => {
+    const cropperRef = useRef(null);
 
-  return (
-    <div>
-      <div style={{ width: "100%" }}>
-        <Cropper
-          ref={cropperRef}
-          style={{ width: "100%" }}
-          zoomTo={1}
-          aspectRatio={1}
-          src={uploadedImage}
-          viewMode={1}
-          background={false}
-          responsive={true}
-          autoCropArea={1}
-          checkOrientation={false}
-          guides={false}
-          data={{ width: '100%' }}
-          crop={getCropData}
-        />
-      </div>
-      <div>
-        <div
-          className="box"
-          style={{ width: "50%", float: "right", height: "300px" }}
-        >
-          <h1>
-            <span>Crop</span>
-            <button style={{ float: "right" }} onClick={getCropData}>
-              Crop Image
-            </button>
-          </h1>
-          <img style={{ width: "100%" }} src={cropData} alt="cropped" />
-        </div>
-      </div>
-      <br style={{ clear: "both" }} />
-    </div>
-  );
+    const getCropData = () => {
+        if (cropperRef.current) {
+            const cropper = cropperRef.current.cropper;
+            if (cropper && cropper.getCroppedCanvas) {
+                const croppedCanvas = cropper.getCroppedCanvas();
+                if (croppedCanvas) {
+                    const dataUrl = croppedCanvas.toDataURL();
+                    setCroppedImage(dataUrl);
+                    closeModal();
+                }
+            }
+        }
+    };
+
+    return (
+        <Modal closeFn={closeModal}>
+            <h2 >이미지 자르기</h2>
+            <Modal.Body>
+                <div className='cropContainer'>
+                    <Cropper
+                        ref={cropperRef}
+                        style={{ width: '100%' }}
+                        zoomTo={1}
+                        aspectRatio={1}
+                        src={uploadedImage}
+                        viewMode={1}
+                        background={false}
+                        responsive={true}
+                        autoCropArea={1}
+                        checkOrientation={false}
+                        guides={false}
+                        data={{ width: '100%' }}
+                    />
+                </div>
+            </Modal.Body>
+            <Modal.Button>
+                <Button type="button" label="자르기" variant="active" onClick={getCropData} />
+            </Modal.Button>
+        </Modal>
+    );
 };
 
 ImageEditor.propTypes = {
-  uploadedImage:PropTypes.node
-}
+    uploadedImage: PropTypes.node,
+    closeModal: PropTypes.func.isRequired,
+    setCroppedImage: PropTypes.func.isRequired
+};
 
 export default ImageEditor;
