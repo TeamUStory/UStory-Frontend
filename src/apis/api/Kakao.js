f542efa9877d611251ad2e615658a6964a0d78c3
Kang KyungRim <kkltr416@gmail.com>
2024-06-15 03:35:33 +0900

    ✨ Feat : 카카오 로그인 메소드 수정

--- src/apis/api/Kakao.js
+++ src/apis/api/Kakao.js
@@ -1,13 +1,13 @@
 export default {
   /**
-   * @method GET
+   * @method POST
    * @summary 카카오 로그인
    * @param {string} code - 카카오 인증 코드
    */
   getKakaoLogin(code) {
     return {
       url: `/login/oauth2/code/kakao?code=${code}`,
-      method: "get",
+      method: "post",
     };
   }
 };
