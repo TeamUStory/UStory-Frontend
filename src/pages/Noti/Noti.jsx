277c61e1015c8e5daa95cc72e4c25c997612859d
Kang KyungRim <kkltr416@gmail.com>
2024-07-04 20:47:52 +0900

    ✨ Feat : 알림 전체 삭제, 선택 삭제 추가

--- src/pages/Noti/Noti.jsx
+++ src/pages/Noti/Noti.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Noti.module.scss';
-import SubHeader from '@/components/SubHeader/SubHeader';
 import NoResult from '@/components/NoResult/NoResult';
 import BellIcon from '@/assets/icons/BellIcon';
 import Button from '@/components/Button/Button';
@@ -12,19 +11,28 @@ import MoreIcon from "@/assets/icons/MoreIcon";
 import Modal from "@/components/Modal/Modal";
 import XImg from "@/assets/images/cancelImage.png";
 import useInfiniteScroll from '@/hooks/useInfiniteScroll';
+import ArrowIcon from "@/assets/icons/ArrowIcon";
+import CheckIcon from "@/assets/icons/CheckIcon";
 
 const Noti = () => {
   const { fetchData: fetchNoti, data: notiData } = useAxios();
   const { fetchData: fetchNotiDelete, response: deleteRes } = useAxios();
+  const { fetchData: fetchNotiDeleteAll, response: allDeleteRes } = useAxios();
+  const { fetchData: fetchNotiDeleteSelect, response: selectDeleteRes } = useAxios();
   const [notiList, setNotiList] = useState([]);
   const [isModal, setIsModal] = useState(false);
+  const [allDelete, setAllDelete] = useState(false);
   const [deleteSuccess, setDeleteSuccess] = useState(false);
+  const [allDeleteSuccess, setAllDeleteSuccess] = useState(false);
   const [activeId, setActiveId] = useState(null);
   const [selectedNoticeId, setSelectedNoticeId] = useState(null); // 선택된 알림 ID 추가
   const [page, setPage] = useState(1); // 페이지 상태 추가
   const [loading, setLoading] = useState(false); // 로딩 상태 추가
   const targetEl = useRef(null); // 무한 스크롤 타겟 요소
   const intersecting = useInfiniteScroll(targetEl); // 무한 스크롤 훅 사용
+  const [isToggle, setIsToggle] = useState(null);
+  const [isSelect, setIsSelect] = useState(false); // 선택 삭제 버튼 활성화
+  const [isCheck, setIsCheck] = useState(false); // 체크박스 클릭 시 삭제하기 버튼 활성화
   const navigate = useNavigate();
 
   // 알림 조회
@@ -137,9 +145,100 @@ const Noti = () => {
     return `${year}.${month}.${dayOfMonth} (${day})`;
   };
 
+  // header에 있는 moreIcon
+  const toggleMenu = () => {
+    setIsToggle(isToggle ? false : true);
+  };
+
+  // 다른 여백 누르면 메뉴 닫기
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (!e.target.closest(`.${styles.moreIcon}`)) {
+        setIsToggle(false);
+      }
+    };
+
+    window.addEventListener("click", handleClickOutside);
+    return () => window.removeEventListener("click", handleClickOutside);
+  }, []);
+
+  // 전체 삭제
+  const handleAllDelete = async () => {
+    await fetchNotiDeleteAll(Notice.deleteNoticeAll());
+    setNotiList([]); // 전체 알림 삭제
+    setAllDelete(false);
+  }
+
+  // 전체 삭제 성공
+  useEffect(() => {
+    if (allDeleteRes && allDeleteRes.status === 204) {
+      setAllDeleteSuccess(true);
+    }
+  }, [allDeleteRes])
+
+  // 선택 삭제 버튼
+  const handleSelectDelete = async () => {
+    // selectedNoticeIds는 선택된 알림 ID들
+    const selectedNoticeIds = [];
+    const checkboxes = document.querySelectorAll(`.${styles.checkbox}`);
+    checkboxes.forEach((checkbox) => {
+      if (checkbox.checked) {
+        selectedNoticeIds.push(Number(checkbox.id.split('_')[1]));
+      }
+    })
+
+    await fetchNotiDeleteSelect(Notice.deleteNoticeSelected({ noticeIds: selectedNoticeIds }));
+  }
+
+  // 선택 삭제 성공
+  useEffect(() => {
+    if (selectDeleteRes && selectDeleteRes.status === 204) {
+      setIsCheck(false);
+      window.location.reload();
+    }
+  }, [selectDeleteRes])
+
+  // 체크박스 클릭 시 삭제하기 버튼 활성화
+  useEffect(() => {
+    const buttonActive = () => {
+      const checkboxes = document.querySelectorAll(`.${styles.checkbox}`);
+      checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener('change', () => {
+          setIsCheck(checkboxes.length > 0 && Array.from(checkboxes).some((checkbox) => checkbox.checked));
+        })
+      })
+    }
+    buttonActive()
+  }, [isSelect])
+
+  // 취소하기
+  const handleCancel = () => {
+    setIsSelect(false);
+    setIsCheck(false);
+    // 체크박스 초기화
+    const checkboxes = document.querySelectorAll(`.${styles.checkbox}`);
+    checkboxes.forEach((checkbox) => {
+      checkbox.checked = false;
+    })
+  }
+
   return (
     <>
-      <SubHeader pageTitle={"알림"} />
+      <div className={styles.header}>
+        <Button type="button" variant="inactive" label={<ArrowIcon fill="#1d1d1d" />} onClick={() => navigate(-1)} />
+        <h1 className={styles.title}>알림</h1>
+        <div className={styles.rightHeader}>
+          <button className={styles.moreIcon} onClick={toggleMenu}>
+            <MoreIcon stroke="black" />
+          </button>
+          {isToggle && (
+            <div className={styles.menuContainer}>
+              <Button type="button" variant="inactive" label="선택 삭제" onClick={() => setIsSelect(true)}/>
+              <button className={styles.exitButton} type="button" onClick={() => setAllDelete(true)}>전체 삭제</button>
+            </div>
+          )}
+        </div>
+      </div>
       <div className={styles.NotiWrap}>
         <div className={styles.wrap}>
           {notiList.length === 0 ? 
@@ -147,6 +246,16 @@ const Noti = () => {
             :
             notiList.map((noti, idx) => (
               <div key={idx} className={styles.NotiBox}>
+                <div className={styles.checkBox}
+                  style={
+                    isSelect ? {display: "block"} : {display: "none"}
+                  }
+                  >
+                  <input type="checkbox" id={`selectDelete_${noti.noticeId}`} name={`selectDelete_${noti.noticeId}`} className={styles.checkbox}/>
+                  <label htmlFor={`selectDelete_${noti.noticeId}`} className={styles.selectDelete}>
+                    <CheckIcon/>
+                  </label>
+                </div>
                 <div className={styles.Noti} onClick={() => handleButton(noti)}>
                   <div className={styles.contentBox}>
                     <p className={clsx(styles.category, {
@@ -158,7 +267,6 @@ const Noti = () => {
                     </p>
                     <p className={styles.content}>{noti.message}</p>
                   </div>
-                  {/* <p className={styles.date}>{noti.time.split("T")[0]}</p> */}
                   <p className={styles.date}>{formatKoreanDate(noti.time)}</p>
                 </div>
                 <div className={styles.btnBox}>
@@ -189,6 +297,30 @@ const Noti = () => {
                 </Modal.Button>
               </Modal>
             )}
+            {allDelete && (
+              <Modal closeFn={() => setAllDelete(false)}>
+                <Modal.Icon><img src={XImg} alt="x"/></Modal.Icon>
+                <Modal.Body>
+                  {allDeleteSuccess ? "삭제되었습니다." : `전체 알림을 삭제하시겠습니까?`}
+                </Modal.Body>
+                <Modal.Button>
+                  {allDeleteSuccess ? 
+                    <Button type="button" label="확인" variant="active" onClick={() => {setAllDelete(false); window.location.reload()}}/>
+                    :
+                    <Button type="button" label="삭제하기" variant="active" onClick={() => handleAllDelete()}/>
+                  }
+                </Modal.Button>
+              </Modal>
+            )}
+        </div>
+        <div className={styles.deleteBtnBox}
+          style={
+            isSelect ? {display: "block"} : {display: "none"}
+          }>
+          <div>
+            <Button type="button" label="취소하기" variant="noFilled" onClick={handleCancel} />
+            <Button type="button" label="삭제하기" variant={isCheck ? "active" : "disabled"} onClick={handleSelectDelete}/>
+          </div>
         </div>
       </div>
     </>
