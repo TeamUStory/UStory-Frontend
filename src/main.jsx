acb4979354091b2f77655eab1fa3aae39549bb11
양혜지 <97512098+hyeji17@users.noreply.github.com>
2024-07-22 11:17:35 +0900

    📦️ Add: react-redux 파일 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
-import Error from '@/components/Error/Error.jsx';  
+import Error from '@/components/Error/Error.jsx';
+import { Provider } from "react-redux";
+import store from '@/redux/store/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ErrorBoundary
     fallbackRender={({ resetErrorBoundary }) => <Error resetErrorBoundary={resetErrorBoundary} />}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
     </ErrorBoundary>
 )
