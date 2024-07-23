d3918f2cb1e1b8eca25b94cebe8786058ae24433
양혜지 <97512098+hyeji17@users.noreply.github.com>
2024-07-24 01:40:36 +0900

    ✨ Add: paperSlice 추가

---
+++ src/redux/slices/paperSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const paperSlice = createSlice({
+    name: "paper",
+    initialState: {
+    },
+    reducers: {
+    },
+});
+
+// export const { } = paperSlice.actions;
+export default paperSlice;
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import homeSlice from "@/redux/slices/homeSlice";
 import diarySlice from "@/redux/slices/diarySlice";
+import paperSlice from "@/slices/paperSlice";
 
 const store = configureStore({
     reducer: {
         home: homeSlice.reducer,
-        diary: diarySlice.reducer
+        diary: diarySlice.reducer,
+        paper: paperSlice.reducer
     }
 })
 
